e16d00e80b74c93dd96eccc1ae8dfefe4ccc3ecb
egeseckin <ege.seckin@outlook.com>
2022-02-12 17:39:18 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Ege Seçkin on 12.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
