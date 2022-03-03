1298cae0da1b57d31164cdea9b0d6f3876db7e8f
egeseckin <ege.seckin@outlook.com>
2022-03-03 14:40:02 +0300

    Email And logo display on login screen

---
+++ Messenger/Controllers/login/RegisterViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterViewController.swift
+//  Messenger
+//
+//  Created by Ege Seçkin on 16.02.2022.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .link
+    }
+
+}
